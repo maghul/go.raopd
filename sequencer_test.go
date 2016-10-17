4f60b760c548a438ee83bf615d396f8ad696317f
Magnus Hulthén <magnus@computer.se>
2016-10-18 00:41:55 +0200

    Changes test to comply with new timing.

--- sequencer_test.go
+++ sequencer_test.go
@@ -9,15 +9,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const sequencerDebug = false
-
 func inSeqRange(in chan *rtpPacket, from, to int) {
 	for ii := from; ii != to+1; ii++ {
 		in <- testPacket(seqno(ii), 0)
 	}
 }
 
-func inSeqs(in chan *rtpPacket, va ...interface{}) {
+func (s *sequencer) inSeqs(in chan *rtpPacket, va ...interface{}) {
 	for _, v := range va {
 		switch v := v.(type) {
 		case int:
@@ -30,55 +28,46 @@ func inSeqs(in chan *rtpPacket, va ...interface{}) {
 	}
 }
 
-func checkSeqNo(t *testing.T, q chan *rtpPacket, expected int) {
-	if sequencerDebug {
-		seqlog.Debug.Println("CHECK SEQNO expected=", expected)
-	}
+func (s *sequencer) checkSeqNo(t *testing.T, q chan *rtpPacket, expected int) {
+	s.sl.note("CHECK SEQNO expected=", expected)
 	select {
 	case p := <-q:
-		if sequencerDebug {
-			seqlog.Debug.Println("CHECK SEQNO received=", p.sn)
-		}
+		s.sl.note("CHECK SEQNO received=", p.sn)
 		assert.NotEqual(t, -1, expected, fmt.Sprintf("Queue should be empty, contained packet with seqno=%d", p.sn))
 		assert.Equal(t, seqno(expected), p.sn)
 	case <-time.After(time.Millisecond * 1):
-		if sequencerDebug {
-			seqlog.Debug.Println("CHECK SEQNO  *empty*")
-		}
+		s.sl.note("CHECK SEQNO  *empty*")
 		assert.Equal(t, -1, expected, fmt.Sprintf("Queue is empty, should contain packet with seqno=%d", expected))
 	}
 }
 
-func checkSeqNos(t *testing.T, q chan *rtpPacket, from, to int) {
-	seqlog.Debug.Println("CHECK SEQNO from=", from, ", to=", to)
+func (s *sequencer) checkSeqNos(t *testing.T, q chan *rtpPacket, from, to int) {
+	s.sl.note("CHECK SEQNO from=", from, ", to=", to)
 	for ii := from; ii <= to; ii++ {
-		checkSeqNo(t, q, ii)
+		s.checkSeqNo(t, q, ii)
 	}
-	checkSeqNo(t, q, -1)
+	s.checkSeqNo(t, q, -1)
 }
 
-func checkReq(t *testing.T, q chan rerequest, expected, count int) {
-	if sequencerDebug {
-		seqlog.Debug.Println("CHECK REREQUEST", expected, "...", expected+count-1)
-	}
+func (s *sequencer) checkReq(t *testing.T, q chan rerequest, expected, count int) {
+	s.sl.note("CHECK REREQUEST: ", expected, "...", expected+count-1)
 	select {
 	case r := <-q:
-		if sequencerDebug {
-			seqlog.Debug.Println("CHECK REREQUEST: received", r)
-		}
+		s.sl.note("CHECK REREQUEST: received", r)
 		assert.NotEqual(t, -1, expected, "Request Queue should be empty")
-		if sequencerDebug {
-			seqlog.Debug.Println("CHECK REREQUEST: ", expected, int(r.first))
-		}
+		s.sl.note("CHECK REREQUEST: ", expected, int(r.first))
 		assert.Equal(t, expected, int(r.first))
 		assert.Equal(t, count, int(r.count))
 	case <-time.After(time.Millisecond * 1):
-		if sequencerDebug {
-			seqlog.Debug.Println("CHECK REREQUEST: *empty")
-		}
+		s.sl.note("CHECK REREQUEST: *empty")
 		msg := fmt.Sprintf("Request was empty, should contain [%d...%d]", expected, expected+count-1)
 		assert.Equal(t, -1, expected, msg)
 	}
+}
+
+func (s *sequencer) sleep(t time.Duration) {
+	s.sl.note("Sleeping: ", t)
+	time.Sleep(t)
 }
 
 func testPacket(sn seqno, payloadType uint8) *rtpPacket {
@@ -106,12 +95,12 @@ func TestSequenceConsecutive(t *testing.T) {
 	in <- testPacket(6, 0)
 	in <- testPacket(7, 0)
 
-	checkSeqNo(t, out, 4)
-	checkSeqNo(t, out, 5)
-	checkSeqNo(t, out, 6)
-	checkSeqNo(t, out, 7)
-	checkSeqNo(t, out, -1)
-	checkReq(t, request, -1, 0)
+	s.checkSeqNo(t, out, 4)
+	s.checkSeqNo(t, out, 5)
+	s.checkSeqNo(t, out, 6)
+	s.checkSeqNo(t, out, 7)
+	s.checkSeqNo(t, out, -1)
+	s.checkReq(t, request, -1, 0)
 
 	s.close()
 }
@@ -132,12 +121,12 @@ func TestSequenceSingleGap(t *testing.T) {
 	in <- testPacket(5, 0)
 	in <- testPacket(7, 0)
 
-	checkSeqNo(t, out, 4)
-	checkSeqNo(t, out, 5)
-	checkSeqNo(t, out, 6)
-	checkSeqNo(t, out, 7)
-	checkSeqNo(t, out, -1)
-	checkReq(t, request, -1, 0)
+	s.checkSeqNo(t, out, 4)
+	s.checkSeqNo(t, out, 5)
+	s.checkSeqNo(t, out, 6)
+	s.checkSeqNo(t, out, 7)
+	s.checkSeqNo(t, out, -1)
+	s.checkReq(t, request, -1, 0)
 
 	s.close()
 }
@@ -158,12 +147,12 @@ func TestSequenceDoubleGap(t *testing.T) {
 	in <- testPacket(6, 0)
 	in <- testPacket(5, 0)
 
-	checkSeqNo(t, out, 4)
-	checkSeqNo(t, out, 5)
-	checkSeqNo(t, out, 6)
-	checkSeqNo(t, out, 7)
-	checkSeqNo(t, out, -1)
-	checkReq(t, request, -1, 0)
+	s.checkSeqNo(t, out, 4)
+	s.checkSeqNo(t, out, 5)
+	s.checkSeqNo(t, out, 6)
+	s.checkSeqNo(t, out, 7)
+	s.checkSeqNo(t, out, -1)
+	s.checkReq(t, request, -1, 0)
 
 	s.close()
 }
@@ -179,31 +168,32 @@ func TestSequenceWideGap(t *testing.T) {
 	}
 	s := startSequencer("test", in, of, request)
 
-	inSeqs(in, []int{46542, 46544})               // 46542..46544
-	inSeqs(in, 46554, 46549, []int{46555, 46559}) // 46542..46544 46549 46554..46559
-	checkSeqNos(t, out, 46542, 46544)             // 46542..46544 <-- 46549 46554..46559
+	s.inSeqs(in, []int{46542, 46544})               // 46542..46544
+	s.inSeqs(in, 46554, 46549, []int{46555, 46559}) // 46542..46544 46549 46554..46559
+	s.checkSeqNos(t, out, 46542, 46544)             // 46542..46544 <-- 46549 46554..46559
 
-	inSeqs(in, 46545, []int{46547, 46553}) // 46545 465474..46559
-	checkSeqNos(t, out, 46545, 46545)      // 46545 <-- 465474..46559
+	s.inSeqs(in, 46545, []int{46547, 46553}) // 46545 465474..46559
+	s.checkSeqNos(t, out, 46545, 46545)      // 46545 <-- 465474..46559
 
-	time.Sleep(11 * time.Millisecond)
-	checkReq(t, request, 46546, 1) // ??1 46546..46546
-	checkReq(t, request, -1, 0)
-	inSeqs(in, []int{46569, 46570}) // 46545 <-- 465474..46559 46569..46570
+	s.sleep(33 * time.Millisecond)
+	s.checkReq(t, request, 46546, 1) // ??1 46546..46546
+	s.checkReq(t, request, -1, 0)
+	s.inSeqs(in, []int{46569, 46570}) // 46545 <-- 465474..46559 46569..46570
 
-	time.Sleep(11 * time.Millisecond)
-	checkReq(t, request, 46560, 9) // ??1 46560..46568
-	checkReq(t, request, -1, 0)
+	s.sleep(30 * time.Millisecond)
+	s.checkReq(t, request, 46560, 9) // ??1 46560..46568
+	s.sl.note("no requests..")
+	s.checkReq(t, request, -1, 0)
 
-	time.Sleep(22 * time.Millisecond)
-	inSeqs(in, 46546)                 // 46546..46559 46569..46570
-	checkSeqNos(t, out, 46546, 46559) // 46546..46559 <-- 46569..46570
-	checkReq(t, request, 46546, 1)    // ??2 46546..46546
-	checkReq(t, request, -1, 0)
+	s.sleep(51 * time.Millisecond)      // 33+30+51=114ms
+	s.inSeqs(in, 46546)                 // 46546..46559 46569..46570
+	s.checkSeqNos(t, out, 46546, 46559) // 46546..46559 <-- 46569..46570
+	s.checkReq(t, request, 46546, 1)    // ??2 46546..46546
+	s.checkReq(t, request, -1, 0)
 
-	time.Sleep(11 * time.Millisecond)
-	checkReq(t, request, 46560, 9) // ??2 46560..46568
-	checkReq(t, request, -1, 0)
+	s.sleep(33 * time.Millisecond)
+	s.checkReq(t, request, 46560, 9) // ??2 46560..46568
+	s.checkReq(t, request, -1, 0)
 
 	s.close()
 }
@@ -219,47 +209,46 @@ func TestSequenceReReRequest(t *testing.T) {
 	}
 	s := startSequencer("test", in, of, request)
 
-	inSeqs(in, []int{46542, 46544})
+	s.inSeqs(in, []int{46542, 46544})
 	// gap 46545..46553
-	inSeqs(in, 46554, 46549, []int{46555, 46559})
+	s.inSeqs(in, 46554, 46549, []int{46555, 46559})
 	// gap 46560..46568
-	inSeqs(in, []int{46569, 46570})
+	s.inSeqs(in, []int{46569, 46570})
 
-	checkSeqNos(t, out, 46542, 46544)
+	s.checkSeqNos(t, out, 46542, 46544)
 
-	time.Sleep(11 * time.Millisecond)
+	s.sleep(31 * time.Millisecond)
 
-	inSeqs(in, 46545, []int{46547, 46553})
-	checkSeqNos(t, out, 46545, 46545)
+	s.inSeqs(in, 46545, []int{46547, 46553})
+	s.checkSeqNos(t, out, 46545, 46545)
 
-	time.Sleep(11 * time.Millisecond)
+	s.sleep(31 * time.Millisecond)
 
-	inSeqs(in, 46570)
+	s.inSeqs(in, 46570)
 
-	checkReq(t, request, 46545, 4)
-	checkReq(t, request, 46550, 4)
-	checkReq(t, request, 46560, 9)
-	checkReq(t, request, -1, 0)
+	s.checkReq(t, request, 46545, 4)
+	s.checkReq(t, request, 46550, 4)
+	s.checkReq(t, request, 46560, 9)
+	s.checkReq(t, request, -1, 0)
 
-	time.Sleep(33 * time.Millisecond)
+	s.sleep(91 * time.Millisecond)
 
-	inSeqs(in, 46546)
-	checkSeqNos(t, out, 46546, 46559)
+	s.inSeqs(in, 46546)
+	s.checkSeqNos(t, out, 46546, 46559)
 
 	// Second rerequests
-	println("SECOND REREQUEST")
-	checkReq(t, request, 46546, 1)
-	checkReq(t, request, 46560, 9)
-	checkReq(t, request, -1, 0)
+	s.sl.note("SECOND REREQUEST")
+	s.checkReq(t, request, 46546, 1)
+	s.checkReq(t, request, 46560, 9)
+	s.checkReq(t, request, -1, 0)
 
-	println("SECOND REREQUEST DONE")
+	s.sl.note("SECOND REREQUEST DONE")
 	s.close()
-	println("TEST DONE")
+	s.sl.note("TEST DONE")
 
 }
 
-func NoTestSequenceSlowResponse(t *testing.T) {
-	seqlog.Debug.Println("TestSequenceDoublegap")
+func TestSequenceSlowResponse(t *testing.T) {
 	in := make(chan *rtpPacket, 10)
 	out := make(chan *rtpPacket, 10)
 	request := make(chan rerequest, 10)
@@ -269,20 +258,20 @@ func NoTestSequenceSlowResponse(t *testing.T) {
 	}
 	s := startSequencer("test", in, of, request)
 
-	inSeqs(in, []int{46542, 46544})
-	inSeqs(in, []int{46547, 46554})
+	s.inSeqs(in, []int{46542, 46544})
+	s.inSeqs(in, []int{46547, 46554})
 
-	checkSeqNos(t, out, 46542, 46544)
-	time.Sleep(11 * time.Millisecond)
+	s.checkSeqNos(t, out, 46542, 46544)
+	s.sleep(31 * time.Millisecond)
 
 	// These are packets coming from the normal data RTP session and should
 	// not retrigger a request which is already pending.
-	inSeqs(in, []int{46555, 46556})
-	time.Sleep(11 * time.Millisecond)
+	s.inSeqs(in, []int{46555, 46556})
+	s.sleep(31 * time.Millisecond)
 
-	checkReq(t, request, 46545, 2)
+	s.checkReq(t, request, 46545, 2)
 	// We should only have a single rerequest here
-	checkReq(t, request, -1, 0)
+	s.checkReq(t, request, -1, 0)
 
 	s.close()
 }
@@ -298,20 +287,20 @@ func TestSequenceMissingRecoveryPacket(t *testing.T) {
 	}
 	s := startSequencer("test", in, of, request)
 
-	inSeqs(in, []int{46542, 46544})
-	inSeqs(in, []int{46547, 46554})
+	s.inSeqs(in, []int{46542, 46544})
+	s.inSeqs(in, []int{46547, 46554})
 
-	checkSeqNos(t, out, 46542, 46544)
-	time.Sleep(11 * time.Millisecond)
+	s.checkSeqNos(t, out, 46542, 46544)
+	s.sleep(33 * time.Millisecond)
 
 	// This packet is a recovery but 46545 is still missing so we should
 	// get a new request for that.
-	inSeqs(in, 46546)
-	time.Sleep(33 * time.Millisecond)
+	s.inSeqs(in, 46546)
+	s.sleep(91 * time.Millisecond)
 
-	checkReq(t, request, 46545, 2)
-	checkReq(t, request, 46545, 1)
-	checkReq(t, request, -1, 0)
+	s.checkReq(t, request, 46545, 2)
+	s.checkReq(t, request, 46545, 1)
+	s.checkReq(t, request, -1, 0)
 
 	s.close()
 }
@@ -327,43 +316,42 @@ func TestSequenceGiveUp(t *testing.T) {
 	}
 	s := startSequencer("test", in, of, request)
 
-	inSeqs(in, []int{46542, 46544}) // 46542..46544
-	inSeqs(in, []int{46547, 46554}) // 46542..46544  46547..46554
+	s.inSeqs(in, []int{46542, 46544}) // 46542..46544
+	s.inSeqs(in, []int{46547, 46554}) // 46542..46544  46547..46554
 
-	checkSeqNos(t, out, 46542, 46544) // 46542..46544  <-- 46547..46554
-	time.Sleep(11 * time.Millisecond)
+	s.checkSeqNos(t, out, 46542, 46544) // 46542..46544  <-- 46547..46554
+	s.sleep(33 * time.Millisecond)
 
 	// This packet is a recovery but 46545 is still missing so we should
 	// get a new request for that.
-	inSeqs(in, 46546) // 46546..46554
+	s.inSeqs(in, 46546) // 46546..46554
 
-	time.Sleep(11 * time.Millisecond)
-	//	checkSeqNos(t, out, 46547, 46554)
-	checkReq(t, request, 46545, 2) // ??1 46545..46546?
-	checkReq(t, request, -1, 0)
+	s.sleep(33 * time.Millisecond)
+	//	s.checkSeqNos(t, out, 46547, 46554)
+	s.checkReq(t, request, 46545, 2) // ??1 46545..46546?
+	s.checkReq(t, request, -1, 0)
 
-	time.Sleep(11 * time.Millisecond)
-	checkReq(t, request, -1, 0)
+	s.sleep(11 * time.Millisecond)
+	s.checkReq(t, request, -1, 0)
 
-	time.Sleep(22 * time.Millisecond)
-	checkReq(t, request, 46545, 1) // ??2 46545..46545?
-	checkReq(t, request, -1, 0)
+	s.sleep(80 * time.Millisecond)
+	s.checkReq(t, request, 46545, 1) // ??2 46545..46545?
+	s.checkReq(t, request, -1, 0)
 
-	time.Sleep(11 * time.Millisecond)
-	checkReq(t, request, -1, 0)
+	s.sleep(11 * time.Millisecond)
+	s.checkReq(t, request, -1, 0)
 
-	time.Sleep(44 * time.Millisecond)
-	checkReq(t, request, 46545, 1) // ??3 46545..46545?
-	checkReq(t, request, -1, 0)
+	s.sleep(80 * time.Millisecond)
+	s.checkReq(t, request, 46545, 1) // ??3 46545..46545?
+	s.checkReq(t, request, -1, 0)
 
-	time.Sleep(11 * time.Millisecond)
-	checkReq(t, request, -1, 0)
+	s.sleep(11 * time.Millisecond)
+	s.checkReq(t, request, -1, 0)
 
-	time.Sleep(55 * time.Millisecond)
-	println("7")
-	checkReq(t, request, -1, 0)
+	s.sleep(55 * time.Millisecond)
+	s.checkReq(t, request, -1, 0)
 	// We should not see any more requests for 46545
-	//checkReq(t, request, -1, 0)
+	//s.checkReq(t, request, -1, 0)
 
 	s.close()
 }
